a4a44b4a2066385bb049bae28ea2bfb3cdc8d066
vasu sridharan <vasusridharan1@gmail.com>
2017-09-01 10:34:34 +1000

    few more added

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
-const songsData = require('./data');
+const songsData = require('./data').songsData;
 
 var port = process.env.PORT || 8000;
 
@@ -24,12 +24,12 @@ restService.use(bodyParser.urlencoded({
 restService.use(bodyParser.json());
 
 restService.post('/', function(req, res) {
- //get from local file
- let song = getRandomValue(songsData.songsData.list).link ;
+  //get from local file
+  let song = getRandomValue(songsData.list).link;
 
- let songsAudioTag =  "<audio src='"+song+"'> That's all for now</audio>";
- let songText =  `<speak>${concat([songsAudioTag])}</speak>`;
- 
+  let songsAudioTag = "<audio src='" + song + "'> That's all for now</audio>";
+  let songText = `<speak>${concat([songsAudioTag])}</speak>`;
+
   return res.json({
     speech: songText,
     displayText: songText,
