a06c3f3fc96c70b8093db5b06386463561dd6b2b
vasu sridharan <vasusridharan1@gmail.com>
2017-09-05 15:21:50 +1000

    added util method back

--- index.js
+++ index.js
@@ -7,6 +7,15 @@ const jsonData = require('./jsonData.json');
 
 var port = process.env.PORT || 8000;
 
+/**
+ * @template T
+ * @param {Array<T>} array The array to get a random value from
+ */
+const getRandomValue = array => array[Math.floor(Math.random() * array.length)];
+
+/** @param {Array<string>} messages The messages to concat */
+const concat = messages => messages.map(message => message.trim()).join(' ');
+
 const restService = express();
 
 restService.use(bodyParser.urlencoded({
@@ -23,7 +32,7 @@ restService.post('/', function(req, res) {
   let song = songs.find(o => o.title.includes(songTitle)).link;
   let songsAudioTag = "<audio src='" + song + "'> That's all for now</audio>";
   let songText = `<speak>${concat([songsAudioTag])}</speak>`;
-  console.log(`playing song ${jsonData.whereAmI} , ${song}`);
+  console.log(`playing song ${jsonData.whereAmI}: ${song}`);
   //update index
   jsonData.whereAmI = jsonData.whereAmI +1;
   fs.writeFileSync('./jsonData.json', JSON.stringify(jsonData));
